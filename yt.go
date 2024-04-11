38cdc60f82f43e7ca9b9a63266b2718e2a23ef24
Lochy <lochois@gmail.com>
2024-04-11 16:19:29 +1000

    fix playlist download

--- yt.go
+++ yt.go
@@ -15,45 +15,76 @@ func GetMedia(link string) error {
 	// Create a youtube client
 	client := youtube.Client{}
 	format := &youtube.Format{}
+	videoIDs := []string{}
 
-	// Extract the video ID from the link
-	videoID, err := youtube.ExtractVideoID(link)
+	// Check if link is a playlist, if its a playlist loop through all videos and append to videoIDs
+	playlist, err := client.GetPlaylist(link)
 	if err != nil {
 		// Return the error if it occurs
-		return fmt.Errorf("error extracting video ID: %v", err)
+		return fmt.Errorf("error getting playlist: %v", err)
 	}
 
-	// Get the video from youtube
-	video, err := client.GetVideo(videoID)
-	if err != nil {
-		// Return the error if it occurs
-		return fmt.Errorf("error getting video: %v", err)
-	}
-
-	// Find a suitable format for the video
-	for i := range video.Formats {
-		switch video.Formats[i].ItagNo {
-		case 251, 250:
-			format = &video.Formats[i]
-			// break
+	if playlist != nil {
+		// Get the first video in the playlist
+		println("Playlist found, number of videos:", len(playlist.Videos))
+		for _, video := range playlist.Videos {
+			// videoID, err := youtube.ExtractVideoID(video.ID)
+			// if err != nil {
+			// 	// Return the error if it occurs
+			// 	return fmt.Errorf("error extracting video ID: %v", err)
+			// }
+			videoIDs = append(videoIDs, video.ID)
 		}
 	}
+	if playlist == nil {
+		// Extract the video ID from the link
+		videoID, err := youtube.ExtractVideoID(link)
+		if err != nil {
+			// Return the error if it occurs
+			return fmt.Errorf("error extracting video ID: %v", err)
+		}
 
-	// Download the video using the selected format
-	err = DownloadVideo(video, format)
-	if err != nil {
-		// Return the error if it occurs
-		return fmt.Errorf("error downloading video: %v", err)
+		videoIDs = append(videoIDs, videoID)
 	}
 
+	go func() {
+		for _, videoID := range videoIDs {
+			// Get the video from youtube
+			video, err := client.GetVideo(videoID)
+			if err != nil {
+				// Return the error if it occurs
+				fmt.Errorf("error getting video: %v", err)
+			}
+
+			// Find a suitable format for the video
+			for i := range video.Formats {
+				switch video.Formats[i].ItagNo {
+				case 251, 250:
+					format = &video.Formats[i]
+					// break
+				}
+			}
+
+			// Download the video using the selected format
+			err = DownloadVideo(video, format)
+			if err != nil {
+				// Return the error if it occurs
+				fmt.Errorf("error downloading video: %v", err)
+			}
+
+		}
+	}()
+
 	return nil
+
 }
 
 func DownloadVideo(video *youtube.Video, format *youtube.Format) error {
 	client := youtube.Client{}
 	re := regexp.MustCompile(`[\\/:*?"<>|]`)
 	videoTitle := re.ReplaceAllString(video.Title, "-")
-	title := "/Music/" + videoTitle + "." + "opus"
+	title := "Music/" + videoTitle + "." + "opus"
+
 	stream, size, err := client.GetStream(video, format)
 	if err != nil {
 		return fmt.Errorf("error getting video stream: %v", err)
